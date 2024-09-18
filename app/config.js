a6716321316b177451cb309bd48a19346e3b00b6
theninetimes <swussl@163.com>
2024-09-18 11:22:58 +0800

    删除隐私相关

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    secret:'',
-    connectionStr:''
+    secret:'10192233x2zzz',
+    connectionStr:'mongodb+srv://theninetimes:10192233x2zzz@zhihu.xlvqa.mongodb.net/?retryWrites=true&w=majority&appName=zhihu'
 }
